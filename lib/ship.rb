503f46f00960e11ec369947d130817165b0dec10
Kristin Weiland <kristinweiland@gmail.com>
2024-10-24 17:08:18 -0700

    Add sunk? and hit methods and corresponding tests.

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,9 +3,42 @@ class Ship
                 :length,
                 :health
 
-    def initialize(name, length, health = length)
+    def initialize(name, length, health = length, sunk = false)
         @name = name
         @length = length
         @health = health
+        @sunk = sunk
+        # @hit = ship.hit
     end
+
+    def sunk?
+        if @health == 0
+            true
+        else
+            false
+        end
+    end
+
+    def hit
+        @health -= 1
+    end
+
+
+    # def commit_heinous_act
+    #     @heinous_act_count +=1
+    # end
+
+    # def cursed?
+    #     if @heinous_act_count >=3
+    #         true
+    #     else
+    #         false
+    #     end
+    # end
+
+
+
+
+
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,4 +1,5 @@
 require './lib/ship.rb'
+require 'pry'
 
 RSpec.describe Ship do 
     describe '#initialize' do
@@ -12,10 +13,53 @@ RSpec.describe Ship do
             expect(cruiser.name).to eq("Cruiser")
             expect(cruiser.length).to eq(3)
             expect(cruiser.health).to eq(3) 
-        end
-        
-
-
+        end       
     end
-       
+
+    describe '#sunk' do 
+        it 'has sunk?/is not suck by default' do
+            cruiser = Ship.new("Cruiser", 3)
+            
+            expect(cruiser.sunk?).to eq(false)
+        end
+    end
+
+    describe '#hit' do 
+        it 'has been hit' do
+            cruiser = Ship.new("Cruiser", 3)
+
+            expect(cruiser.sunk?).to eq(false)
+
+            cruiser.hit
+            expect(cruiser.health).to eq(2)
+            expect(cruiser.sunk?).to eq(false)
+
+            cruiser.hit
+            expect(cruiser.health).to eq(1)
+            expect(cruiser.sunk?).to eq(false)
+
+            cruiser.hit
+            expect(cruiser.health).to eq(0)
+            expect(cruiser.sunk?).to eq(true)
+        end
+    end     
+            # expect(cruiser.sunk?).to eq(false)
+
 end
+
+
+# it 'is not cursed by default' do
+#     pirate = Pirate.new('Jack')
+
+#     expect(pirate.cursed?).to be false
+
+#     pirate.commit_heinous_act
+#     expect(pirate.cursed?).to be false
+
+#     pirate.commit_heinous_act
+#     expect(pirate.cursed?).to be false
+
+#     pirate.commit_heinous_act
+#     expect(pirate.cursed?).to be true
+#   end
+
