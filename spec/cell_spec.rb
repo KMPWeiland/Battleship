f38317369137e5ec383b79c949a1b2bac42898b1
Shadeau Christensen <Shadeau100@gmail.com>
2024-10-25 08:53:47 -0700

    Added intitialized method and test, Empty?method and test, Ship method and test

---
+++ spec/cell_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require './lib/ship.rb'
+require './lib/cell.rb'
+require 'pry'
+
+RSpec.describe Cell do
+    describe '#initialize' do
+        it 'exists' do
+            cell = Cell.new("B4")
+            expect(cell).to be_a(Cell)
+        end
+        
+        it 'has attributes' do
+            cell = Cell.new("B4")
+            expect(cell.coordinate).to eq("B4")
+        end
+
+        it 'has a ship' do
+            cell = Cell.new("B4")
+            expect(cell.ship).to eq(nil)
+        end
+    end
+
+    describe '#empty?' do
+        it 'cell is empty?' do
+            cell = Cell.new("B4")
+            expect(cell.empty?).to eq(true)
+        end
+
+        it 'placed ship' do
+            cell = Cell.new("B4")
+            cruiser = Ship.new("Cruiser", 3)
+
+            cell.place_ship(cruiser)  
+            expect(cell.ship).to eq(cruiser)
+            expect(cell.empty?).to eq(false)
+        end
+
+    end
+        
+end
